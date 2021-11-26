4b179eec8e1e47f7bad89bf9e9ce4831db6e8cbe
gohara23 <59593124+gohara23@users.noreply.github.com>
2021-11-26 00:55:47 -0500

    create globals class

---
+++ globals.py
@@ -0,0 +1,16 @@
+import os
+
+class Globals():
+    def __init__(self):
+        self.TIME_FOR_EXECUTION = 45
+        self.CREDS_PATH = "C:\Users\gabri\OneDrive - nd.edu\theta_bot\config.json"
+        self.DB_PATH = "database.db"
+        self.DATA_COLLECT_STOCKS = ["TSLA", "AMD", "HUT", "AMC", "PLTR", "MARA", "GOEV", "RIOT", "BITO", "GME"]
+        self.DATA_COLLECT_CRYPTO = ["BTC", "ETH", "DOGE"]
+        # self.DB_PATH = os.getcwd()+"/db/"
+
+
+if __name__ == "__main__":
+    globals = Globals()
+    print(globals.DB_PATH)
+
