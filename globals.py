82fb490cf9d46a61c73d9642a98dc286342f553a
gohara23 <59593124+gohara23@users.noreply.github.com>
2021-11-28 15:18:15 -0500

    Add format and standard path

--- globals.py
+++ globals.py
@@ -1,16 +1,14 @@
-import os
 
 class Globals():
     def __init__(self):
         self.TIME_FOR_EXECUTION = 45
-        self.CREDS_PATH = r"C:\Users\gabri\OneDrive - nd.edu\theta_bot\config.json"
+        self.CREDS_PATH = r"config.json"
         self.DB_PATH = "database.db"
         self.DATA_COLLECT_STOCKS = ["TSLA", "AMD", "HUT", "AMC", "PLTR", "MARA", "GOEV", "RIOT", "BITO", "GME"]
         self.DATA_COLLECT_CRYPTO = ["BTC", "ETH", "DOGE"]
         self.DATA_COLLECT_FREQUENCY = 300 # seconds
 
+        self.DATE_FORMAT = "%Y-%m-%d"
+        self.CHECK_MARKET_HOURS = False
 
-if __name__ == "__main__":
-    globals = Globals()
-    print(globals.DB_PATH)
 
