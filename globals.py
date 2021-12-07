15358617b4d7e149a84060330cf8a1ad35e654a2
gohara23 <59593124+gohara23@users.noreply.github.com>
2021-12-07 13:43:56 -0500

    update globals

--- globals.py
+++ globals.py
@@ -1,14 +1,21 @@
 
-class Globals():
-    def __init__(self):
-        self.TIME_FOR_EXECUTION = 45
-        self.CREDS_PATH = r"config.json"
-        self.DB_PATH = "database.db"
-        self.DATA_COLLECT_STOCKS = ["TSLA", "AMD", "HUT", "AMC", "PLTR", "MARA", "GOEV", "RIOT", "BITO", "GME"]
-        self.DATA_COLLECT_CRYPTO = ["BTC", "ETH", "DOGE"]
-        self.DATA_COLLECT_FREQUENCY = 300 # seconds
+TIME_FOR_EXECUTION = 45
+CREDS_PATH = r"config.json"
+DB_PATH = "database.db"
+DATA_COLLECT_STOCKS = ["SPY", "DIA", "QQQ", "TSLA", "AMD",
+                       "HUT", "AMC", "PLTR", "MARA", "GOEV", "RIOT", "BITO", "GME"]
+DATA_COLLECT_CRYPTO = ["BTC", "ETH", "DOGE"]
+DATA_COLLECT_FREQUENCY = 60  # seconds
 
-        self.DATE_FORMAT = "%Y-%m-%d"
-        self.CHECK_MARKET_HOURS = False
+DATE_FORMAT = "%Y-%m-%d"
+CHECK_MARKET_HOURS = True
+EXTENDED_HOURS_STOCKS = True
 
 
+CRYPTO_PAIRS = [
+    "BTC-USD", "ETH-USD", "DOGE-USD", "ADA-USD"]
+
+STOCK_TABLE = "stocks"
+CRYPTO_TABLE = "crypto"
+OPTIONS_TABLE = "options"
+PORTFOLIO_TABLE = "portfolio"
